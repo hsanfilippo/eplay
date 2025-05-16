b8af95f5618d765837f4152b150abbdbed21026e
Henrique Sanfilippo <henrique.sanfilippo@gmail.com>
2025-05-16 15:42:20 -0300

    feat: Props para estilizacao do background dos cards de acordo com a section

--- src/components/ProductsList/index.tsx
+++ src/components/ProductsList/index.tsx
@@ -2,13 +2,13 @@ import Product from '../Product'
 
 import { Container, List } from './styles'
 
-type Props = {
+export type Props = {
   title: string
   background: 'gray' | 'black'
 }
 
 const ProductsList = ({ background, title }: Props) => (
-  <Container>
+  <Container background={background}>
     <div className="container">
       <h2>{title}</h2>
       <List>
