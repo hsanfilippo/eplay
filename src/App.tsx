269631f4998624ce2599f2a2872771ca2d321089
Henrique Sanfilippo <henrique.sanfilippo@gmail.com>
2025-05-16 11:43:36 -0300

    chore: exibindo o componente de listagem no router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
+import { GlobalCss } from './styles'
 import Banner from './components/Banner'
 import Header from './components/Header'
-import { GlobalCss } from './styles'
+import Product from './components/Product'
+import ProductsList from './components/ProductsList'
 
 const rotas = createBrowserRouter([
   {
     path: '/',
-    element: <Banner />
+    element: (
+      <>
+        <Banner />
+        <ProductsList title="Promoções" background="gray" />
+      </>
+    )
   }
 ])
 
