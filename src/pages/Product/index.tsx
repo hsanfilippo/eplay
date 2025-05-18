9502e8387d178350bd86be6b53f4f872ba791276
Henrique Sanfilippo <henrique.sanfilippo@gmail.com>
2025-05-18 12:57:38 -0300

    feat: componentizando as sections

--- src/pages/Product/index.tsx
+++ src/pages/Product/index.tsx
@@ -1,5 +1,6 @@
 import { useParams } from 'react-router-dom'
 import Hero from '../../components/Hero'
+import Section from '../../components/Section'
 
 const Product = () => {
   const { id } = useParams()
@@ -7,6 +8,33 @@ const Product = () => {
   return (
     <>
       <Hero />
+      <Section title="Sobre o jogo" background="black">
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad ipsa
+          ipsum qui ratione veniam ullam velit, nobis est corporis quasi iste
+          voluptatibus officia. Minus atque magni quia cum asperiores corporis!
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad ipsa
+          ipsum qui ratione veniam ullam velit, nobis est corporis quasi iste
+          voluptatibus officia. Minus atque magni quia cum asperiores corporis!
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad ipsa
+          ipsum qui ratione veniam ullam velit, nobis est corporis quasi iste
+          voluptatibus officia. Minus atque magni quia cum asperiores corporis!
+        </p>
+      </Section>
+      <Section title="Mais detalhes" background="gray">
+        <p>
+          <b>Plataforma:</b> Playstation 5 <br />
+          <b>Desenvolvedor:</b> Avalanche Software <br />
+          <b>Editora:</b> Portkey Games, subsidiária da Warner Bros. Interactive
+          Entertainment <br />
+          <b>Idiomas:</b> Inglês, espanhol, francês, alemão, italiano,
+          português. As opções de áudio e legenda podem ser ajustadas nas
+          configurações do jogo.
+        </p>
+      </Section>
+      <Section title="Galeria" background="black">
+        <div>fotos</div>
+      </Section>
     </>
   )
 }
